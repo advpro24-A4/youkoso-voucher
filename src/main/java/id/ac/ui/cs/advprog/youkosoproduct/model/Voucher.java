58928523769a8281c32e754e007459f0c491b70f
raviehasan <ravie.hasan@ui.ac.id>
2024-04-24 20:59:13 +0700

    [RED] Add Voucher model skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/youkosoproduct/model/Voucher.java
@@ -0,0 +1,36 @@
+package id.ac.ui.cs.advprog.youkosoproduct.model;
+
+public class Voucher {
+    private String id;
+    private String name;
+    private double discountAmount;
+    private int maxUsage;
+
+    public String getId() {
+        return null;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public double getDiscountAmount() {
+        return 0;
+    }
+
+    public int getMaxUsage() {
+        return 0;
+    }
+
+    public void setId(String id) {
+    }
+
+    public void setName(String name) {
+    }
+
+    public void setDiscountAmount(double discountAmount) {
+    }
+
+    public void setMaxUsage(int maxUsage) {
+    }
+}
