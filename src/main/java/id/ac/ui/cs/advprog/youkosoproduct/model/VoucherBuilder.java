b04da62276a73688cfac99fe8316425e115902af
raviehasan <ravie.hasan@ui.ac.id>
2024-05-01 20:41:46 +0700

    [GREEN] implement builder design pattern on voucher model

---
+++ src/main/java/id/ac/ui/cs/advprog/youkosoproduct/model/VoucherBuilder.java
@@ -0,0 +1,51 @@
+package id.ac.ui.cs.advprog.youkosoproduct.model;
+
+public class VoucherBuilder {
+    private String name;
+    private double discountPercentage;
+    private Boolean hasUsageLimit;
+    private int usageLimit = Integer.MAX_VALUE;
+    private double minimumOrder = 0.0;
+    private int maximumDiscountAmount = Integer.MAX_VALUE;
+
+    public VoucherBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public VoucherBuilder discountPercentage(double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+        return this;
+    }
+
+    public VoucherBuilder hasUsageLimit(Boolean hasUsageLimit) {
+        this.hasUsageLimit = hasUsageLimit;
+        return this;
+    }
+
+    public VoucherBuilder usageLimit(int usageLimit) {
+        this.usageLimit = usageLimit;
+        return this;
+    }
+
+    public VoucherBuilder minimumOrder(double minimumOrder) {
+        this.minimumOrder = minimumOrder;
+        return this;
+    }
+
+    public VoucherBuilder maximumDiscountAmount(int maximumDiscountAmount) {
+        this.maximumDiscountAmount = maximumDiscountAmount;
+        return this;
+    }
+
+    public Voucher build() {
+        Voucher voucher = new Voucher();
+        voucher.setName(this.name);
+        voucher.setDiscountPercentage(this.discountPercentage);
+        voucher.setHasUsageLimit(this.hasUsageLimit);
+        voucher.setUsageLimit(this.usageLimit);
+        voucher.setMinimumOrder(this.minimumOrder);
+        voucher.setMaximumDiscountAmount(this.maximumDiscountAmount);
+        return voucher;
+    }
+}
