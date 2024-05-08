84cab3b1a2bb4b4de23d0dc3d38318e31507e9c7
raviehasan <ravie.hasan@ui.ac.id>
2024-05-08 21:04:09 +0700

    [RED] add main method test to maximize coverage

--- src/test/java/id/ac/ui/cs/advprog/youkosoproduct/YoukosoProductApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/youkosoproduct/YoukosoProductApplicationTests.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.youkosoproduct;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,13 @@ class YoukosoProductApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true, "The context loads successfully");
+    }
+
+    @Test
+    void mainMethodTest() {
+        String[] arr = new String[] {};
+        YoukosoProductApplication.main(arr);
     }
 
 }
